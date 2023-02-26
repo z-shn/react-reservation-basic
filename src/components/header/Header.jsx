3ad0bc4c11842f25ac001da65a0b3a14ba9cee2d
z-shn <zshn.code@gmail.com>
2023-02-26 21:54:57 +0500

    skeleton-navbar-header basic

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,73 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBed,
+  faCar,
+  faPlane,
+  faTaxi,
+} from '@fortawesome/free-solid-svg-icons';
+import './header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car Rentals</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport Taxis</span>
+          </div>
+        </div>
+        <h1 className="headerTitle">A lifetime of discouts? It's a Genius.</h1>
+        <p className="headerDesc">
+          Get rewarded for your traels - unlock instant savings of 10% or more
+          with a free booking account.
+        </p>
+        <button className="headerButton">Sign in / Register</button>
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <input
+              type="text"
+              placeholder="where are you going?"
+              className="headerSearchInput"
+            />
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <input
+              type="text"
+              placeholder="where are you going?"
+              className="headerSearchInput"
+            />
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <input
+              type="text"
+              placeholder="where are you going?"
+              className="headerSearchInput"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,14 @@
+import Navbar from '../../components/navbar/Navbar';
+import Header from '../../components/header/Header';
+import './home.css';
+
 export default function Home() {
-  return <div>HOME COMPONENT</div>;
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Header />
+      </div>
+    </>
+  );
 }
